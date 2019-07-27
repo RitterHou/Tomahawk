2323855b7dc5058b614a94917b287e104291e62a
侯锐 <hourui@qianmi.com>
2019-07-27 18:19:25 +0800

    修改编译脚本

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-flags="-X 'main.buildStamp=$(date '+%Y-%m-%d %H:%M:%S')' -X main.version=v1.0.0 -X 'main.goVersion=$(go version)'"
+flags="-s -w -X 'main.buildStamp=$(date '+%Y-%m-%d %H:%M:%S')' -X main.version=v1.0.0 -X 'main.goVersion=$(go version)'"
 # echo "Build flags: ${flags}"
 
-go build -ldflags "$flags" -o Tomahawk
+start_time=`date +%s`
+
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "$flags" -o Tomahawk.exe
+CGO_ENABLED=0 GOOS=linux   GOARCH=amd64 go build -ldflags "$flags" -o Tomahawk_Linux
+CGO_ENABLED=0 GOOS=darwin  GOARCH=amd64 go build -ldflags "$flags" -o Tomahawk_Darwin
+
+end_time=`date +%s`
+cost=$[$end_time-$start_time]
+echo "Build success, cost ${cost} second(s)"

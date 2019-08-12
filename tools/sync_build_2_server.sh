83aae6780b29ddcdade8ab099690e216b4eec0e6
侯锐 <hourui@qianmi.com>
2019-08-12 16:46:43 +0800

    添加目标文件自动同步脚本

---
+++ tools/sync_build_2_server.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# 编译生成目标文件并且文件同步到测试服务器
+
+cd $(dirname ${0}) # shell文件路径
+cd ..              # 工程根目录
+
+./build.sh -c 'linux:amd64'
+
+declare -a servers=('172.21.34.245' '172.21.34.202' '172.21.3.92')
+
+for server in "${servers[@]}"
+do
+    echo "Sync ${server}"
+    rsync -avz -e 'ssh -p 22' ./Tomahawk_linux_amd64 root@${server}:/home/tomahawk
+    ssh root@${server} 'chown -R tomahawk:tomahawk /home/tomahawk/Tomahawk_linux_amd64'
+done
